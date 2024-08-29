d74eaeab5d4b9dc469c84da336bba430376ac86c
Андрей Комаров <Shadowonfire7@gmail.com>
2024-08-29 17:18:40 +0400

    Исправлены ошибки в коде

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -4,9 +4,9 @@ import model.Task;
 import java.util.List;
 
 public interface HistoryManager {
-    void addAll (Task task);
+    void addAll(Task task);
 
-    void remove (int id);
+    void remove(int id);
 
     List<Task> getHistory();
 }
